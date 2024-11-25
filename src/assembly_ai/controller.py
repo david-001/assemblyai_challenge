f2ac408cecc49142462c3306f26089de7e8a72d8
David Akim <davidjohnakim@hotmail.com>
2024-11-24 20:00:08 -0400

    Version 1

--- src/assembly_ai/controller.py
+++ src/assembly_ai/controller.py
@@ -1,6 +1,7 @@
 import assemblyai as aai
 from dotenv import load_dotenv
 import os
+import re
 
 # Load environment variables from .env file
 load_dotenv()
@@ -24,3 +25,43 @@ def transcribe(audio_file):
             return transcript.text
     except:
         print(f"Transcription failed: {transcript.error}")
+
+
+def summarize(audio_file):
+    transcriber = aai.Transcriber()
+    try:
+        # Step 1: Transcribe an audio file.
+        transcript = transcriber.transcribe(audio_file)
+        # # Step 2: Define a summarization prompt.
+        prompt = "Provide a brief summary of the transcript."
+        # Step 3: Apply LeMUR.
+        result = transcript.lemur.task(
+            prompt, final_model=aai.LemurModel.claude3_5_sonnet
+        )
+        return result.response
+    except:
+        print(f"Summarization failed")
+
+
+def diarization(audio_file):
+    config = aai.TranscriptionConfig(
+        speaker_labels=True,
+    )
+
+    try:
+        transcript = aai.Transcriber().transcribe(audio_file, config)
+        response = ""
+        for utterance in transcript.utterances:
+            response += (f"Speaker {utterance.speaker}: {utterance.text} \n")
+        return response
+    except:
+        print(f"Diarization failed")
+
+
+def count_words(text):
+    # Use regex to find all words (ignores punctuation)
+    words = re.findall(r'\b\w+\b', text)
+
+    # Count the number of words
+    word_count = len(words)
+    return word_count
--- src/main.py
+++ src/main.py
@@ -1,28 +1,45 @@
 from taipy.gui import Gui, State, download
-from assembly_ai.controller import transcribe
+from assembly_ai.controller import transcribe, summarize, diarization
 import io
 
 audio_file = ""
 transcript_text = ""
 
+transcript_options = ["Transcribe", "Summarize", "Diarization"]
+selected_transcript_option = transcript_options[0]
+title = selected_transcript_option
+
+
 page = """
-# Taipy Transcriber
+# Sophisticated Speech-to-Text
+
+<|{selected_transcript_option}|toggle|lov={transcript_options}|on_change=transcript_option_change|>
+<|card|
+<|title|
+<|{title}|>
+|>
 
 Load Audio: <|{audio_file}|file_selector|label=Select File|on_action=load_audio_action|extensions=.wav,.mp3|drop_message=Drop Message|>
 
 Transcript:
-<|card
-<|{transcript_text}|>
+<|transcript-card|
+<|{transcript_text}|text|mode=pre|>
 |>
 
-
 <|{None}|file_download|label=Download File|on_action=download_action|>
+
+|>
 
 """
 
 
 def load_audio_action(state):
-    state.transcript_text = transcribe(state.audio_file)
+    if state.selected_transcript_option == "Transcribe":
+        state.transcript_text = transcribe(state.audio_file)
+    elif state.selected_transcript_option == "Summarize":
+        state.transcript_text = summarize(state.audio_file)
+    elif state.selected_transcript_option == "Diarization":
+        state.transcript_text = diarization(state.audio_file)
 
 
 def download_action(state):
@@ -33,5 +50,11 @@ def download_action(state):
             buffer.getvalue(), "UTF-8"), name="transcript.txt")
 
 
+def transcript_option_change(state):
+    state.title = state.selected_transcript_option
+    state.audio_file = ""
+    state.transcript_text = ""
+
+
 if __name__ == "__main__":
-    Gui(page=page).run(title="Taipy Transcriber")
+    Gui(page=page).run(title="Sophisticated Speech-to-Text")
